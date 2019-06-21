07750c3127279233057b7ce352a9d6815b42e5be
chengw <878899580@qq.com>
2019-06-21 08:23:24 +0800

    cloud

--- tiafs/src/main/java/com/chengw/tiafs/TiafsApplication.java
+++ tiafs/src/main/java/com/chengw/tiafs/TiafsApplication.java
@@ -2,12 +2,10 @@ package com.chengw.tiafs;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 /**
  * @author chengw
@@ -15,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @MapperScan(basePackages = "com.chengw.tiafs.dao")
 @ServletComponentScan
+@EnableDiscoveryClient
 public class TiafsApplication {
 
     public static void main(String[] args) {
