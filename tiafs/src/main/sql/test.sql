e21956c6eddf06e5fb2e81a990c6e21eb55913db
veaxcw <878899580@qq.com>
2019-09-29 20:43:29 +0800

    mv tiafs

--- tiafs/src/main/sql/test.sql
+++
@@ -1,3 +0,0 @@
-update research set publishreader = 'nature',rname='photoelectronic' where rid = 1
-insert into researchresult(rrid, rrname, rrpath, publishdate, publishreader, uploaddate, summary, usercode) VALUES
-(1,'name','rpath','2018-03-02','nat','2019-05-6','good','1')
